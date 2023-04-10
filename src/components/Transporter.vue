fff5b9ad1d0f4eceedec1e7d1d066520e1df25ab
horst7000 <124horst@gmail.com>
2023-04-11 01:25:48 +0200

    added ship-warehouse switch

--- src/components/Transporter.vue
+++ src/components/Transporter.vue
@@ -7,7 +7,7 @@
     position: sticky;
     bottom: 0;
     margin: 0 var(--main-margin);
-    height: 100px;
+    height: 150px;
     border-top: 3px solid var(--color-border);
     border-bottom: none;
     border-top-left-radius: 35px;
@@ -15,9 +15,22 @@
     background: black;
     box-shadow: inset 0 -1em 3em var(--color-background), 0.3em 0.3em 1em var(--color-border);
   ">
-  
+    <br style="height: 5rem;"/>
+    <div class="flex">
+      <div style="flex-grow: 0;">
+        <label class="stack">
+          <input type="radio" name="source" checked>
+          <span class="button toggle pseudo">Ship</span>
+        </label>
+        <label class="stack">
+          <input type="radio" name="source">
+          <span class="button toggle pseudo">Warehouse</span>
+        </label>
+      </div>
+    </div>
+
     <div class="flex center">
-      <button style="flex-grow: 0.5; background: var(--color-background-mute); color: var(--color-text);">Move here ...</button>
+      <button style="flex-grow: 0.7; padding: 0; background: var(--color-background-mute); color: var(--color-text);">Move here ...</button>
     </div>
   </div>
 </template>
