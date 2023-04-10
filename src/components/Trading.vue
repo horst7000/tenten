a5380c54261f222c60051d9e928ec013e40d6efd
horst7000 <124horst@gmail.com>
2023-04-11 01:25:00 +0200

    added more trading placeholders

--- src/components/Trading.vue
+++ src/components/Trading.vue
@@ -1,13 +1,12 @@
 <template>
   <h1>Trading</h1>
-  <div class="flex five" v-for="i in 23" >
-    <p class="three-fifth">Ressource {{ i }}</p>
-    <div class="two-fifth flex three">
-      <button :disabled="Math.random()<0.5" v-if="selected"></button>
-      <p>{{ Math.floor(30*Math.random()) }}</p>
-      <button :disabled="Math.random()<0.5" v-if="selected"></button>      
-    </div>
-
+  <div class="flex nine" v-for="i in 23" >
+    <span class="label warning" :style="{ backgroundColor: `hsl(${15*i}, 60%, 50%)`}"></span>
+    <span>{{ Math.random()<0.2 ? rand(30) : '' }}</span>
+    <span class="name two-fifth">Ressource {{ i }}</span>
+    <button :disabled="Math.random()<0.5" v-if="selected" style="padding: 0; line-height: 1;">{{ rand(30) }}</button>
+    <span>{{ rand(30) }}</span>
+    <button :disabled="Math.random()<0.5" v-if="selected" style="padding: 0; line-height: 1;">{{ rand(30) }}</button>
   </div>
 </template>
 
@@ -21,17 +20,30 @@ import { ref } from 'vue';
 const selected = ref(true);
 /* ---------------- computed ---------------- */
 /* ---------------- functions --------------- */
+function rand(max) {
+  return Math.floor((max+1)*Math.random());
+}
 /* ---------------- watchers ---------------- */
 </script>
 
 <style scoped>
 .flex>* {
   padding-bottom: 0;
+  padding-left: 0;
+}
+.name {
+  text-align: initial;
+}
+span {
+  text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
 }
 button {
   background-color: var(--color-background-mute);
 }
 button:disabled {
   background-color: var(--color-background-soft);
+  color: #777;
 }
 </style>
